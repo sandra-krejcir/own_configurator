ffe6967ae2679a827a89208a2417b9e66300b0e7
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-10-12 15:43:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+let selectedColor = "yellow";
+const features = {
+  horns: false,
+  neckCrystals: false,
+  legCrystals: false,
+  wingCrystals: false,
+  tailCrystals: false,
+  saddle: false,
+};
+
+document.addEventListener("DOMContentLoaded", start);
+
+async function start() {
+  let response = await fetch("theDragon-01.svg");
+  let mySvgData = await response.text();
+  document.querySelector("#dragonPart").innerHTML = mySvgData;
+  document
+    .querySelectorAll(".feature-option")
+    .forEach((option) => option.addEventListener("click", toggleOption));
+  init();
+}
+
+function setColor(element, color) {
+  element.style.fill = color;
+}
+
+function init() {
+  document.querySelector("#body").addEventListener("click", (event) => {
+    setColor(document.querySelector("#body"), selectedColor);
+    setColor(document.querySelector("#wrong"), "white");
+  });
+
+  document.querySelector("#wings").addEventListener("click", (event) => {
+    setColor(document.querySelector("#wings"), selectedColor);
+  });
+
+  document.querySelector("#eye").addEventListener("click", (event) => {
+    setColor(event.target, selectedColor);
+  });
+
+  document.querySelector("#colorWheel").addEventListener("input", (event) => {
+    selectedColor = event.target.value;
+    console.log(selectedColor);
+  });
+
+  document.querySelectorAll(".dragon-color").forEach((elm) => {
+    elm.addEventListener("click", (event) => {
+      selectedColor = event.target.style.backgroundColor;
+      console.log(event.target.style.backgroundColor);
+      console.log(selectedColor);
+    });
+  });
+}
+
+function toggleOption(event) {
+  const target = event.currentTarget;
+  const feature = target.dataset.feature;
+  if (features[feature] === false) {
+    features[feature] = true;
+  } else {
+    features[feature] = false;
+  }
+
+  if (features[feature]) {
+    target.classList.add("chosen");
+    console.log(features[feature]);
+    document
+      .querySelector(`[data-feature='${feature}'`)
+      .classList.remove("hide");
+  } else {
+    features[feature] = false;
+    target.classList.remove("chosen");
+    document.querySelector(`[data-feature='${feature}'`).classList.add("hide");
+  }
+}
